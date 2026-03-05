c654dbb8389246db8e9427be87829028b9a346e3
zaiyan-umer <zaiyanumer6@gmail.com>
2026-03-05 22:33:56 +0500

    fix(auth): remove token from response body

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -75,7 +75,6 @@ export const login = async (req: Request, res: Response) => {
         res.status(200).json({
             message: 'User logged-in successfully',
             user: userWithoutPassword,
-            token
         });
     }
     catch (err) {
