d082c9620a607ce0c0715183afdbd5ebe500ed70
zaiyan-umer <zaiyanumer6@gmail.com>
2026-03-05 06:31:26 +0500

    feat(db): add user schema

---
+++ backend/src/db/schema/user.schema.ts
@@ -0,0 +1,19 @@
+import {
+    pgTable, uuid, varchar, timestamp
+} from 'drizzle-orm/pg-core'
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod'
+
+export const users = pgTable("users", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    email: varchar("email", { length: 255 }).notNull().unique(),
+    firstName: varchar("first_name", { length: 50 }),
+    lastName: varchar("last_name", { length: 50 }),
+    username: varchar("username", { length: 50 }),
+    password: varchar("password", { length: 255 }).notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow()
+})
+
+export type User = typeof users.$inferSelect
+export const insertUserSchema = createInsertSchema(users)
+export const selectUserSchema = createSelectSchema(users)
