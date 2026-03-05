8976464b839e24c680ab51917759ba2964334703
zaiyan-umer <zaiyanumer6@gmail.com>
2026-03-05 07:42:09 +0500

    feat(app): add global middleware - cors and helmet

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,20 @@
 import express from 'express'
 import cors from 'cors'
+import helmet from 'helmet'
 import authRoutes from "./routes/auth.routes";
+import env from './config/env';
 
 const app = express();
 
-app.use(cors());
+app.use(helmet())
+app.use(
+    cors({
+        origin: env.CORS_ORIGIN,
+        credentials: true
+    }));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/health', (req, res) => {
     return res.status(200).json({
