33b2f09862a82e41ca5b3328365c5a5ae112977a
zaiyan-umer <zaiyanumer6@gmail.com>
2026-03-07 03:29:52 +0500

    fix(types): add global Express namespace augmentation for user payload

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -2,14 +2,15 @@ import express from 'express';
 import { forgotPassword, getCurrentUser, login, logout, register, resetPassword } from '../controllers/auth.controller';
 import { validateBody } from '../middleware/validation.middleware';
 import { forgotPasswordSchema, loginSchema, registerSchema, resetPasswordSchema } from '../zod/schema';
+import { verifyToken } from '../middleware/verifyToken.middleware';
 
 const router = express.Router();
 
+router.post('/me', verifyToken, getCurrentUser)
 router.post('/register', validateBody(registerSchema), register)
 router.post('/login', validateBody(loginSchema), login)
 router.post('/logout', logout)
 router.post('/forgot-password', validateBody(forgotPasswordSchema), forgotPassword)
 router.post('/reset-password', validateBody(resetPasswordSchema), resetPassword)
-router.get('/auth/me', getCurrentUser)
 
 export default router;
