e6634ab011058a8f4e2af38c82e4d0d6ada6f040
Sana-Munir-Alam <sanamuniralam25@gmail.com>
2026-03-16 13:53:50 +0500

    Removed the commented old version

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,32 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-// import './index.css'
-// import App from './App.tsx'
-// import { Toaster } from 'sonner'
-
-// // Create a client
-// const queryClient = new QueryClient({
-//   defaultOptions: {
-//     queries: {
-//       refetchOnWindowFocus: false,
-//       retry: 1,
-//       staleTime: 5 * 60 * 1000, // 5 minutes
-//     },
-//   },
-// })
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <QueryClientProvider client={queryClient}>
-//       <App />
-//       <Toaster position="top-center" richColors />
-//     </QueryClientProvider>
-//   </StrictMode>,
-// )
-
-
-
 /**
  * src/main.tsx
  *
