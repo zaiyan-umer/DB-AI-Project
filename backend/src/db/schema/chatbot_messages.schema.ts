685e1aff561b9874fa60aa5e44372e877fe2d8a7
zaiyan-umer <zaiyanumer6@gmail.com>
2026-03-25 07:55:43 +0500

    chore: add chatbot table

---
+++ backend/src/db/schema/chatbot_messages.schema.ts
@@ -0,0 +1,25 @@
+import {
+    pgTable,
+    pgEnum,
+    timestamp,
+    uuid, varchar
+} from 'drizzle-orm/pg-core'
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod'
+import users from './user.schema'
+
+export const chatbotMessageRoleEnum = pgEnum('chatbot_message_role', ['user', 'assistant'])
+
+export const chatbot_messages = pgTable("chatbot_messages", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    userId: uuid("user_id").notNull().references(() => users.id),
+    role: chatbotMessageRoleEnum("role").notNull(),
+    content: varchar("content", { length: 255 }).notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow()
+})
+
+export type ChatbotMessage = typeof chatbot_messages.$inferSelect
+export type NewChatbotMessage = typeof chatbot_messages.$inferInsert
+export const insertChatbotMessageSchema = createInsertSchema(chatbot_messages)
+export const selectChatbotMessageSchema = createSelectSchema(chatbot_messages)
+
+export default chatbot_messages
