811bbd19a787785c7f42c46024036e0feb84b9dc
Zaiyan <zaiyanumer6@gmail.com>
2026-05-09 13:25:51 +0500

    feat: update logo on login screen

--- frontend/src/components/ui/logo.tsx
+++ frontend/src/components/ui/logo.tsx
@@ -1,10 +1,9 @@
-import { Sparkles } from 'lucide-react'
-
 export const Logo = () => {
     return (
         < div className="flex items-center gap-3 mb-4" >
-            <div className="w-10 h-10 bg-linear-to-r from-[#667eea] to-[#764ba2] rounded-lg hidden md:flex items-center justify-center">
-                <Sparkles className="w-5 h-5 text-white" />
+
+            <div className='flex justify-center items-center '>
+                <img src="/logo.webp" alt="logo" className='size-7' />
             </div>
 
             <h1 className="text-xl leading-normal! font-bold bg-linear-to-r from-[#667eea] to-[#764ba2] bg-clip-text text-transparent">
